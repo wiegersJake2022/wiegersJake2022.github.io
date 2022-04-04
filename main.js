4ca54a7280bd8ffbb7b98a85e6afcbe3701f6653
wiegerJake2022 <824005#@mystma.org>
2022-04-04 12:46:42 -0500

    update5

---
+++ main.js
@@ -0,0 +1,10 @@
+function strawberry() {
+	document.getElementById("hello").innerHTML = "Strawberry";
+}
+
+document.getElementById("button").addEventListener("click", strawberry);
+
+function changered(){
+	document.getElementById("hello").style.backgroundColor = "red";
+}
+document.getElementById("button").addEventListener("click",changered);
