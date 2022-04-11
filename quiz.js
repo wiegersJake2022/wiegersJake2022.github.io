8adff3fb5b271d51a5f5881e11258520101a4e75
wiegerJake2022 <824005#@mystma.org>
2022-04-11 12:31:53 -0500

    update9

---
+++ quiz.js
@@ -0,0 +1,9 @@
+function checkq1(){
+	if(document.getElementById("q1").value=="7028")
+	{
+		document.getElementById("q1result").innerHTML="correct";
+}
+else{
+	document.getElementById("q1result").innerHTML="wrong";
+}
+}
