e37baff58af561beff6c741f28c0b2ef8b756fdb
wiegerJake2022 <824005#@mystma.org>
2022-04-13 11:31:49 -0500

    count

--- quiz.js
+++ quiz.js
@@ -1,10 +1,27 @@
+var q1= document.getElementById("q1");
+var q2= document.getElementById("q2");
+var q3= document.getElementById("q3");
+var q4= document.getElementById("q4");
+var q5= document.getElementById("q5");
+var submit = document.getElementById("submit");
+var score = document.getElementById("score");
+var result = 0;
 
-function checkq1(){
-	if(document.getElementById("q1").value=="7028")
-	{
-		document.getElementById("q1result").innerHTML="correct";
+function calculate() {
+	if (q1.value == "7028"){
+		result = result + 1;
+	}
+	if (q2.value =="all"){
+		result = result + 1;
+	}
+	if (q3.value =="football"){
+		result = result + 1;
+	}
+	if (q4.value=="Ps4"){
+		result = result + 1;
+	}
+	if (q5.value=="all"){
+		result = result + 1;
+	}
 }
-else{
-	document.getElementById("q1result").innerHTML="wrong";
-}
-}
+document.getElementById("").addEventListener("click",);
